6e0a7d8ae077c62831a7c59367f91161f22c8d56
Alex Holloway <alex@alexholloway.me.uk>
2026-04-13 21:09:16 +0000

    Various deployment tweaks

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,6 +7,11 @@ output "vps_ip" {
   value       = hcloud_primary_ip.main.ip_address
 }
 
+output "vps_ipv6" {
+  description = "IPv6 address bound on the VPS (first address in the assigned /64)"
+  value       = hcloud_server.main.ipv6_address
+}
+
 output "volume_id" {
   description = "Hetzner volume ID (used to construct /dev/disk/by-id/scsi-0HC_Volume_<id>)"
   value       = hcloud_volume.pgdata.id
