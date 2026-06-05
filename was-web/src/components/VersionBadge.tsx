bb8da158efb2bb120d706519c4d553ad2fa98944
Alex Holloway <KaiEkkrin@users.noreply.github.com>
2026-06-05 14:07:41 +0100

    Move about 20260605 (#362)

--- was-web/src/components/VersionBadge.tsx
+++ was-web/src/components/VersionBadge.tsx
@@ -1,19 +1,24 @@
 import './VersionBadge.css';
+import { Link } from 'react-router-dom';
 import packageJson from '../../package.json';
 
 function VersionBadge() {
   const versionString = `v${packageJson.version}+${__GIT_COMMIT__}`;
 
   return (
-    <a
-      href="https://github.com/KaiEkkrin/wallandshadow"
-      className="version-badge"
-      target="_blank"
-      rel="noopener noreferrer"
-      title="View on GitHub"
-    >
-      {versionString}
-    </a>
+    <div className="version-badge">
+      <Link to="/about" className="version-badge-link">About</Link>
+      {' | '}
+      <a
+        href="https://github.com/KaiEkkrin/wallandshadow"
+        className="version-badge-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="View on GitHub"
+      >
+        {versionString}
+      </a>
+    </div>
   );
 }
 
